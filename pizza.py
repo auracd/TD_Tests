067f49ebe163ffdb1464e9978c64a7723f450177
aurelie <boxpromail@gmail.com>
2024-03-06 14:12:53 +0100

    new modif

--- pizza.py
+++ pizza.py
@@ -4,3 +4,11 @@ class Pizza() :
         self._ingredients = []
         self._prix = 0 
     
+    def get_name(self) : 
+        return self._name
+    
+    def get_ingredients (self) : 
+        return self._ingredients
+    
+    def get_prix(self) : 
+        return self._prix
